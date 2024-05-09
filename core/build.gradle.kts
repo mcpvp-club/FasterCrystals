bd2b89cd1357bc671bfdda879b4917ce6a04ceec
Jyguy <jskekkx@gmail.com>
2024-05-08 23:43:30 -0400

    build: set autoprovision JDK of core to 21

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
