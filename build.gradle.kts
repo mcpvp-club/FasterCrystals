071e397c2c607a762f75090174e8c8e24494a413
Jyguy <jskekkx@gmail.com>
2024-08-12 12:12:03 -0400

    build: update userdev

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@
 
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.7.1" apply false
+    id("io.papermc.paperweight.userdev") version "1.7.2" apply false
 }
 
 group = "xyz.reknown.fastercrystals"
