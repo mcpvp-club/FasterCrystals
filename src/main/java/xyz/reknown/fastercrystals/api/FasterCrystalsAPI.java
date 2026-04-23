1599208f7ed7f8882927be9b3f741bcd844376d7
Jyguy <jskekkx@gmail.com>
2026-04-23 19:22:06 -0400

    chore: update copyright year

--- src/main/java/xyz/reknown/fastercrystals/api/FasterCrystalsAPI.java
+++ src/main/java/xyz/reknown/fastercrystals/api/FasterCrystalsAPI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2025 Jyguy
+ * Copyright (C) 2023-2026 Jyguy
  *
  * FasterCrystals is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/xyz/reknown/fastercrystals/repository/CrystalRepository.java
+++ src/main/java/xyz/reknown/fastercrystals/repository/CrystalRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2025 Jyguy
+ * Copyright (C) 2023-2026 Jyguy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package xyz.reknown.fastercrystals.repository;
 
 import org.bukkit.World;
